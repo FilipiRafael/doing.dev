d77ab56cc45427cd7a89c8a28f63a8d65bd61041
Filipi Rafael <filipirafael.123@gmail.com>
2022-07-28 16:57:38 -0300

    Add new features on todoist

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,8 @@ import './Home.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import LogoutIcon from '@mui/icons-material/Logout';
 import PlaylistAddCheckIcon from '@mui/icons-material/PlaylistAddCheck';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const Home = () => {
 
@@ -10,18 +12,18 @@ const Home = () => {
         {
             id: '73180',
             description: 'Go to Coffe',
-            isChecked: false,
+            isChecked: true,
         },        
         {
             id: '19192',
             description: 'Create a React Native Tutorial',
-            isChecked: false,
+            isChecked: true,
         },        
         {
             id: '94920',
             description: 'Make a Youtube Channel',
             isChecked: false,
-        },      
+        },     
     ]);
 
     return (
@@ -45,9 +47,16 @@ const Home = () => {
                                 {tasks.map(task => (
                                     <div>
                                         <input type="checkbox" />
-                                        {task.description}
+                                        {task.isChecked ?
+                                            <span className='home__tasks-line-through'>{task.description}</span> :
+                                            <span>{task.description}</span>
+                                        }
+                                        <DeleteIcon className="home__tasks-deleteicon" />
                                     </div>
                                 ))}
+                                <button className="home__tasks-button">
+                                    <AddIcon />
+                                </button>
                             </div>
                         ) :
                         (
