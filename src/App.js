521ffb98223c2d1f304ad911fb84cdaa564f22a5
Filipi Rafael <filipirafael.123@gmail.com>
2022-07-29 13:37:27 -0300

    new adjustes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import Home from "./pages/Home";
-import SignIn from './pages/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <Home />
     </div>
   );
 }
